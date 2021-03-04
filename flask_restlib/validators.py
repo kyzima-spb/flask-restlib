509f67bcc3558b432560dcc265b0907f9f07e140
Kirill Vercetti <office@kyzima-spb.com>
2021-03-04 18:10:19 +0300

    Added a validator that checks the existence of an entity in the database.

--- flask_restlib/validators.py
+++ flask_restlib/validators.py
@@ -11,8 +11,42 @@ __all__ = (
     'Validator', 'ValidationError',
     'URL', 'Email', 'Range', 'Length', 'Equal', 'Regexp', 'Predicate',
     'NoneOf', 'OneOf', 'ContainsOnly', 'ContainsNoneOf',
-    'UniqueEntity',
+    'ExistsEntity', 'UniqueEntity',
 )
+
+
+class ExistsEntity(Validator):
+    """
+    The validator checks that an entity with the specified attributes exists.
+    """
+
+    default_message = 'An instance with {value} attributes does not exist.'
+
+    def __init__(self, model_class, fields='id', resource_manager=None, error=None):
+        """
+        Arguments:
+            model_class:
+                Reference to the model class.
+            fields (str|iterable):
+                Model attributes that must be unique.
+            resource_manager:
+                Custom resource manager.
+                If not specified, it is used by default from the extension.
+            error (str):
+                Error message.
+        """
+        self.model_class = model_class
+        self.fields = (fields,) if isinstance(fields, str) else fields
+        self.resource_manager = resource_manager
+        self.error = error or self.default_message
+
+    def __call__(self, *values):
+        resource_manager = self.resource_manager or current_restlib.ResourceManager(self.model_class)
+        criteria = dict(zip(self.fields, values))
+
+        if not resource_manager.exists(criteria):
+            value = ', '.join(f'{n}={v}' for n, v in criteria.items())
+            raise ValidationError(self.error.format(value=value))
 
 
 class UniqueEntity(Validator):
@@ -20,7 +54,7 @@ class UniqueEntity(Validator):
     The validator checks the attributes of an entity for uniqueness.
     """
 
-    default_message = 'Instance with unique {value} attributes already exists.'
+    default_message = 'An instance with unique {value} attributes already exists.'
 
     def __init__(self, model_class, fields, entity=None, resource_manager=None, error=None):
         """
