f211dab00ba845f48ec1edf0a5e94575eb20a5e4
Kirill Vercetti <office@kyzima-spb.com>
2022-04-24 04:35:45 +0300

    Update example

--- examples/todo/resource-server/instance/config.py
+++ examples/todo/resource-server/instance/config.py
@@ -1,4 +1,4 @@
-MONGODB_HOST = 'mongodb://user:demo@db/todo?authSource=admin'
+MONGODB_HOST = 'mongodb://user:demo@mongo/todo?authSource=admin'
 
 RESTLIB_PAGINATION_ENABLED = False
 # RESTLIB_SORTING_ENABLED = False
